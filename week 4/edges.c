0846b9d91ae0198760347aba8faeb20773adb590
garyan50 <100048754+garyan50@users.noreply.github.com>
2022-09-15 10:52:54 -0400

    Add files via upload

---
+++ week 4/edges.c
@@ -0,0 +1,35 @@
+/*
+addEdge should add an edge from specified node 1 to specified node 2
+    if there is not an edge currently and should print a message if there is already such an edge.
+printOrderedPairs is a function that should print a list of all the ordered pairs (i,j) such that
+    there is an edge from node i to node j.
+printAdjacentNodes should print a list of all nodes j such that there is an edge from the specified
+    node i to j.
+areConnected should return a value of 1 if there is an edge from specified node 1 to specified
+    node 2 and of 0 otherwise.
+*/
+#include <stdio.h>
+#define NODES 20
+int graphEdges;
+
+// add an edge of node1 and node2
+void addEdge(int node1, int node2);
+// print the graph in edge pairs
+void printOrderedPairs(int *graph);
+// return 1 if the two nodes are connected, 0 otherwise
+int areConnected(int node1, int node2);
+
+int main() {}
+
+// add an edge of node1 and node2
+void addEdge(int node1, int node2)
+{
+}
+// print the graph in edge pairs
+void printOrderedPairs(int *graph)
+{
+}
+// return 1 if the two nodes are connected, 0 otherwise
+int areConnected(int node1, int node2)
+{
+}
